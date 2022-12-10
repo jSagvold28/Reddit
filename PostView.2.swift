bc1e3bd290e332708de0a81e915273b951073ccd
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-09 20:36:32 -0600

    Create PostView.2.swift

---
+++ PostView.2.swift
@@ -0,0 +1,80 @@
+//
+//  PostView.2.swift
+//  Reddit
+//
+//  Created by Jayce Sagvold on 12/8/22.
+//
+
+import SwiftUI
+
+struct PostView_2: View {
+    var body: some View {
+        
+        VStack {
+            
+           // let UpvoteOrange : Color = Color(red: 232, green: 103, blue: 13)
+            
+            // RGB VALUES:
+            //rgb(232, 103, 13)
+            
+            VStack {
+                
+                Text("__________________________________________________")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
+            Text("Whats the diffrence in the MX Master 3 and 3S?")
+                .font(.subheadline)
+                .padding(.bottom, 15)
+                .padding(.bottom, 5)
+            
+            HStack {
+                
+                Image(systemName: "arrow.up")
+                   // .foregroundColor("UpvoteOrange")
+                    .padding(.horizontal, 9)
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                
+                Text("4")
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                
+                
+                Image(systemName: "message")
+                    .padding(.horizontal, 9)
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                
+                
+                Text("6")
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                
+                Text("By: Developer5035")
+                    .font(.system(size: 14))
+                    //.bold() // may use agian later
+                    .padding(.bottom, 5)
+                
+                Text("logitech")
+                    .font(.subheadline)
+                    .padding(.bottom, 5)
+                    
+            }
+            
+            VStack {
+                
+                Text("__________________________________________________")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct PostView_2_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView_2()
+    }
+}
